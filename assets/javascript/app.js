b9b8cca51acf44243534c4d3fa0c077647d8f7bb
Patrick Knight <ptknight00@gmail.com>
2018-01-07 14:30:28 -0600

    working app update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,94 @@
+$(document).ready(function(){
 
+
+//variables
 var apiKey = "X97PzdwnxCU8mvCqTGElRK0D1hU0gpGM" //my key
 var limit = 10; //limit
 var rating = "pg"; //gif rating
 var searchTarget = "";//tag
-var actorArray = ["Arnold"];
+var actorArray = ["Joaquin Phoenix", "Emma Stone", "Ryan Gosling", "Amy Adams", "Val Kilmer", "Cate Blanchett", "Samuel L. Jackson", "Meryl Streep", "Tom Cruise", "Tom Hanks"];
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey + "&limit=" + limit + "&rating=" + rating + "&tag=" + "arnold";
-// q
-// limit 
-// rating
-function runSearch(limit, queryURL) {
+
+function display() {
+
+var name = $(this).attr("data-name");
+//search endpoint
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=X97PzdwnxCU8mvCqTGElRK0D1hU0gpGM&limit=" + limit + "&rating=" + rating;
+console.log(queryURL);
+
+// ajax call for clicked button
 $.ajax({
-    url: queryURL,
-    method: "GET"
+  url: queryURL,
+  method: "GET"
 })
 
-.done(function(response){
-    //save image URL proprety
-    console.log("url: " + queryURL);
-    console.log(response);
-    var imageURL = response.data.image_originalurl;
-    //create and store image tag
+.done(function(response) {
+  var results = response.data;
+  $("#gif-section").empty();
+  console.log(response);
+  console.log(results);
+
+  for (var i = 0; i < results.length; i++) {
+    var gifDiv = $("<div class='result'>");
+    var rating = results[i].rating; //find rating
+    var ratingText = $("<p>").text("Rating: " + rating);//display rating
     var actorImage = $("<img>");
-    //set image src to 
-    actorImage.attr("src", imageUrl);
-    actorImage.attr("alt", searchTarget +" actor image");
-    // prepend
-    $(".images").prepend(actorImage);
+    actorImage.addClass("gif");
 
-})
+    actorImage.attr({
+      "src": results[i].images.fixed_height_still.url,
+      "data-still": results[i].images.fixed_height_still.url,
+      "data-animate": results[i].images.fixed_height.url,
+      "data-state": "still"
+    });
+
+    gifDiv.append(ratingText);
+    gifDiv.append(actorImage);
+    $("#gif-section").prepend(gifDiv);
+  }
+});
+
+}
+//create buttons from actorArray
+function renderButton() {
+$('#buttons').empty();
+for (var i = 0; i < actorArray.length; i++) {
+    var createNewButtonArray = $("<button>");
+    createNewButtonArray.addClass("actor");
+    createNewButtonArray.attr("data-name", actorArray[i]);
+    createNewButtonArray.text(actorArray[i]);
+    $('#buttons').append(createNewButtonArray);
+    }
 }
 
-$(document).ready(function() {
-    $("#add-button").on('click', function(){
-        //add input from form to API call
-        // grab 10 static, non-animated gif images from the GIPHY API and place them on the page
-        // display gif rating
-        // add class to gif, define start/stop parameters
-            // clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
-    
-        })
-})
+//add actor
+$('#add-actor').on("click", function(event) {
+  event.preventDefault();
+  //grab the input from the textbox
+  var newActor = $('#actor-input').val().trim();
+  console.log(newActor);
+  //stop creating empty button
+  if (!(newActor == '')) {
+    } else {
+  alert("Please Fill In The Animal You Want To See.");
+  newActor.stop()
+  }
+  actorArray.push(newActor);
+  renderButton();
+});
+
+$(document).on('click', '.actor', display);
+renderButton();
+
+$(document).on('click', '.gif', function() {
+  var state = $(this).attr("data-state");
+
+  if (state === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+  } else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still");
+  };
+});
+});
